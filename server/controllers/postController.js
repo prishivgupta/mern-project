a478005780a66686548b736ae94bf67d1b51e7e2
prishivgupta <55000380+prishivgupta@users.noreply.github.com>
2021-08-10 17:28:38 +0530

    Added the edit post function

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import Posts from "../models/postModel.js";
 
 export const getPosts = async (req,res) => {
@@ -33,4 +34,17 @@ export const createPosts = async (req,res) => {
 
     }
 
+}
+
+export const updatePost = async (req,res) => {
+
+    const {id: _id} = req.params;
+
+    const post = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id");
+
+    const updatedPost = await Posts.findByIdAndUpdate(_id, {...post, _id}, {new: true});
+
+    res.json(updatedPost);
 }
--- server/routers/postsRouter.js
+++ server/routers/postsRouter.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { getPosts, createPosts } from "../controllers/postController.js";
+import { getPosts, createPosts, updatePost } from "../controllers/postController.js";
 
 const postRouter = express.Router();
 
 postRouter.get('/', getPosts);
 postRouter.put('/', createPosts);
+postRouter.patch('/:id',updatePost);
 
 export default postRouter;
